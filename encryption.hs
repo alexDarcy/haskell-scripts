f138782d3834c710e4c52fabf86ace820cd64209
Alexis Praga <alexis.praga@cerfacs.fr>
2014-11-06 18:25:46 +0000

    Using cabal to configure the project. More generic "examine" function but how can we change the file really exists ?

--- encryption.hs
+++ encryption.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE ExtendedDefaultRules #-}
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 
+module Main where
+
 import Shelly
 import Control.Applicative
 import Data.String
@@ -9,28 +11,37 @@ import Data.Foldable
 import qualified Data.Text as T
 default (T.Text)
 
+
+-- | Symmetric encryption
 encrypt :: String -> Sh()
 encrypt x = run_ "gpg" ["-o", T.pack (x ++ ".gpg")
-                       , "--passphrase-file", "./.passphrase_files"
+                       , "--passphrase-file", "~/.passphrase_files"
                        , "-c", T.pack x]
 
-examine :: [String] -> Sh()
-examine files = 
+-- | Symmetric decryption
+decrypt :: String -> Sh()
+decrypt x = run_ "gpg" ["-o", T.pack x
+                       , "--passphrase-file", "~/.passphrase_files"
+                       , "-d", T.pack $ x ++ ".gpg"]
+
+-- | Apply an encryption/decryption function to a list of files
+examine :: (String -> Sh()) -> [String] -> Sh()
+examine f files = 
   forM_ files $ \cur -> do
     exists <- (test_f . fromText . T.pack) cur
-    run_ "echo" ["lol", "kikoo"]
-    if exists then encrypt cur else error $ cur++"do not exist"
+    if exists then f cur else echo $ T.pack $ cur ++ " do not exist"
 
+-- | Encrypt or decrypt with GPG a list of files
+main :: IO()
 main = shelly $ verbosely $ do
-  let files = [ "mail_config/.fetchmailrc2"
+  let files = [ "mail_config/.fetchmailrc"
               , "mail_config/.msmtprc"
               , "mail_config/.procmailrc"
               , "abook/.abook/addressbook"
               , "irssi/.irssi/config"]
-  let folder = "dotfiles/"
-  let paths = map ("dotfiles/" ++) files
+  let paths = map ("~/dotfiles/" ++) files
 
   -- T.pack converts a string to a text
-  examine paths
+  examine encrypt paths
   
-  echo "lol"
+  echo "Done"
