6ad303349ee0815f1f3e84253f78b6e818985732
Alexis Praga <alexis.praga@free.fr>
2019-09-01 11:09:55 +0200

    The parser works for multi-lines

--- stages/Main.hs
+++ stages/Main.hs
@@ -1,16 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
--- Read a PDF file to extract count redundant data. 
--- This is not a good example as the pdf layout is similar to a table and some
--- manual preprocessing is needed.
--- Still, it serves as an example for attoparsec usage
-
+-- Example of a parser with attoparsec
+-- Data is created from a pdf with : pdftotex -layout
+--
+-- Structure :
+-- ID NAME PLACE BOSS
+-- ID = a letter followed by 3 digits
+-- name = a set of characters up to a place
+-- place = search into a list of places *and* 2 spaces after (importsant !)
+-- boss = everything else
 module Main where
 
--- Version non optimale : les données sont créées avec "pdftotex -layout lol.pdf"
--- On s'assure que l'espace entre les colonnes est d'au moins 2
--- Puis on utilise le preprocessing (cf preprocessing.sh) en vérifiant qu'il n'y
--- a pas eu de lignes de perdues
 
+import Control.Applicative
 import Data.Either
 import Data.Attoparsec.Text
 import Data.Attoparsec.Combinator
@@ -50,42 +51,30 @@ parseID  = do
   id2 <- decimal
   return $ T.concat [T.singleton id1, T.pack . show $ id2]
 
-listPlaces = [
-  "C.H.S."
-  , "C.H.G."
-  , "C.C.E.G."
-  , "H.C."
-  , "C.H."
-  , "H.B."
-  , "I.C.L."
-  , "Bel Air"
-  , "C.H."
-  , "C.H.R."
-  , "S.J."
-  , "H.B."
-  , "H.E."
-  , "C.H."
-  , "Cabinet médical"
-  , "Mat"
-  , "C.H."
-  , "H.M.M."
-  , "H.C."
-  , "St-Charles"
-  , "H.E."
-  , "I.R.R."
-  , "C.P.N."
-  , "S.J."]
+listPlaces = ["C.C.E.G."
+             , "C.H."
+             , "C.H.G."
+             , "C.H.R."
+             , "C.H.S."
+             , "C.P.N."
+             , "Cabinet médical"
+             , "Bel Air"
+             , "H.B."
+             , "H.C."
+             , "H.E."
+             , "H.M.M."
+             , "I.C.L."
+             , "I.R.R."
+             , "Mat"
+             , "S.J."
+             , "St-Charles"]
 
+-- Always two spaces after the place (otherwise the name can co)
 isPlace = choice $ map string l
   where l = map (\x -> T.append (T.pack x) (T.pack "  ")) listPlaces
 
 
--- Structure :
--- ID NAME PLACE BOSS
--- ID = a letter followed by 3 digits
--- name = a set of characters up to a place
--- place = search into a list of places *and* 2 spaces after (importsant !)
--- boss = everything else
+
 parseJob :: Parser Job
 parseJob = do
   id <- parseID
@@ -98,17 +87,33 @@ parseJob = do
   boss <- many1 letter
   return $ Job id  s' place (T.strip . T.pack $ boss)
 
--- parseJobs = many $ parseJob <* endOfLine
--- runTest = parseOnly parseJobs $ T.unlines [line1, line2]
+testLines = manyTill anyChar (lookAhead (string "C.H."))
+
+-- If a line is not a job (i.e reading a Job fails), this allows us to continue
+skipTill :: (Alternative f) => f a -> f b -> f b
+skipTill skippedAction nextAction =
+    nextAction
+    <|> skippedAction *> skipTill skippedAction nextAction
+
+skipLine = takeTill isEndOfLine <* endOfLine
+
+-- We cannot read line by line as some Jobs are multi-line.....
+parseAllJobs = many (skipLine `skipTill` parseJob)
+--
+line1 = T.unlines $ ["E185     Cardiologie générale et réadaptation CV - Médecine polyvalente Hayange"
+                    ,"                                                                            C.H.R.           KHALIFÉ"
+                    ]
+runTest = parseOnly parseJob line1
+
+readData = TIO.readFile "test.txt"
 
 main = do
-  file <- TIO.readFile "test.txt"
-  -- let all = fromRight [] (parseOnly parseJobs file)
-  -- Try to read a job by each line
-  let all = map (parseOnly parseJob) $ T.lines file
-  -- Filter non matching lines
-  let all' =  rights all
-  let output =  map printEntry $ countIdentical all'
-  let output' = T.unlines $ T.pack "service;nbpostes" : output
-  print output'
-  TIO.writeFile "output.csv" $ output'
+  file <- readData
+  -- We cannot read line by line as some Jobs are multi-line.....
+  let all = fromRight [] $ (parseOnly parseAllJobs file)
+  -- print all
+  -- Count identical entries
+  let output =  map printEntry $ countIdentical all
+  -- -- let output' = T.unlines $ T.pack "service;nbpostes" : output
+  -- print output
+  TIO.writeFile "output.csv" $ T.unlines output
