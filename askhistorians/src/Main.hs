34f697900007a613c8643b04551324663f70cae7
Alexis Praga <alexis.praga@free.fr>
2021-02-22 22:13:32 +0100

    Getting the access token in haskell

--- askhistorians/src/Main.hs
+++ askhistorians/src/Main.hs
@@ -19,25 +19,30 @@
 -- main :: IO ()
 -- main = haskellLastModifiedDateTime
 
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
 module Main (main) where
 
 import Control.Monad.IO.Class
 import Data.Aeson
+import GHC.Generics
 import Network.HTTP.Req
 import qualified Text.URI as URI
 import Data.Maybe (fromJust)
 -- import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C
 
+data Token = Token { access_token :: String } deriving (Generic, Show)
+
+instance FromJSON Token
 main :: IO ()
 main = do
   loginRaw <- readFile "login.conf"
   let login = lines loginRaw
-  getToken login
+  t <- getToken login
+  print t
 
-getToken :: [String] -> IO()
+getToken :: [String] -> IO Token
 getToken login = runReq defaultHttpConfig $ do
   uri <- URI.mkURI "https://www.reddit.com/api/v1/access_token"
   let (url, options) = fromJust (useHttpsURI uri)
@@ -47,6 +52,6 @@ getToken login = runReq defaultHttpConfig $ do
         "password" =: (login !! 1) <>
         basicAuth (C.pack $ login !! 2) (C.pack $ login !! 3) <>
         header "User-Agent" "freebsd:askhistorians:v1.2.3 (by /u/clumskyKnife)"
-        -- options                         <> -- contains the ?foo=bar part
-        -- port 443 -- here you can put any port of course
-  liftIO $ print (responseBody r :: Value)
+  pure (responseBody r :: Token)
+
+-- -H "Authorization: bearer J1qK1c18UUGJFAzz9xnH56584l4" -A "ChangeMeClient/0.1 by YourUsername" https://oauth.reddit.com/api/v1/me
